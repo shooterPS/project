82404db31dc20371ebee22e6f794b42bf70bb76f
shooterPS <113467180+shooterPS@users.noreply.github.com>
2024-03-27 18:45:35 +0530

    Update App.js

--- ccl/src/App.js
+++ ccl/src/App.js
@@ -11,5 +11,29 @@ class App extends Component{
 	);
 }
 }
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
+export default withAuthenticator(App);
 
 export default App;
