00120e111db6cb50391732348e2c1923f1db6762
shooterPS <113467180+shooterPS@users.noreply.github.com>
2024-03-28 22:17:56 +0530

    Update App.js

--- ccl/src/App.js
+++ ccl/src/App.js
@@ -3,14 +3,6 @@
 import { Component } from 'react';
 import './App.css';
 import Game from './components/Game';
-
-class App extends Component{ 
-	render(){
-	return (
-	<Game/>
-	);
-}
-}
 import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
 import {
@@ -22,18 +14,18 @@ import {
   Card,
 } from "@aws-amplify/ui-react";
 
-function App({ signOut }) {
-  return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
+class App extends Component { 
+  render() {
+    return (
+      <View className="App">
+        <Card>
+          <Image src={logo} className="App-logo" alt="logo" />
+          <Heading level={1}>We now have Auth!</Heading>
+        </Card>
+        <Button onClick={this.props.signOut}>Sign Out</Button>
+      </View>
+    );
+  }
 }
 
 export default withAuthenticator(App);
-
-export default App;
