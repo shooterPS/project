a6d8bc72f2b64342aec9002d9bd73cf956eb757f
shooterPS <113467180+shooterPS@users.noreply.github.com>
2024-04-03 22:50:29 +0530

    Create aws-exports.js

---
+++ ccl/src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsmobile = {
+  "aws_project_region": "your-project-region",
+  "aws_cognito_identity_pool_id": "your-identity-pool-id",
+  "aws_cognito_region": "your-cognito-region",
+  "aws_user_pools_id": "your-user-pool-id",
+  "aws_user_pools_web_client_id": "your-user-pool-web-client-id",
+};
+
+export default awsmobile;
